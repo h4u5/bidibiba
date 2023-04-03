a4ff5497eff21ff452cc24d0eca66a35913eb072
h4u5 <taylorlgroves@gmail.com>
2023-04-03 09:46:29 -0700

    Adding an example PM GPU and CPU run script (2k) processes).

---
+++ run_pm_cpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=2048
+#SBATCH -C cpu
+#SBATCH -q regular
+#SBATCH -A nstaff
+#SBATCH -J bidibiba-cpu
+#SBATCH --mail-user=tgroves@lbl.gov
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:15:00
+
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+
+#run the application:
+srun --ntasks=2048 --ntasks-per-node=1 --cpu_bind=cores /global/cfs/cdirs/m888/tgroves/network_benchmarking/bidibiba/bidibiba_ss
---
+++ run_pm_gpu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -N 512
+#SBATCH -C gpu
+#SBATCH --gpus-per-node=4
+#SBATCH --exclusive
+#SBATCH -A nstaff_g
+#SBATCH -J bidibiba-GPU
+#SBATCH --mail-user=tgroves@lbl.gov
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:15:00
+
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+export MPICH_OFI_NIC_POLICY=NUMA
+export MPICH_OFI_NUM_NICS=4
+
+
+#run the application:
+srun --ntasks=2048 --ntasks-per-node=4 --cpu_bind=cores /global/cfs/cdirs/m888/tgroves/network_benchmarking/bidibiba/bidibiba_ss
